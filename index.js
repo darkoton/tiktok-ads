159bd7e5871c34d3953a63ead65fa79db8d6dbcb
darkoto12 <darkoto12@gmail.com>
2024-12-10 17:58:00 +0200

    rename file static

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use(express.static('template'));
+app.use(express.static('static'));
 
 
 app.get("/data", async (req, res)=>{
